c33c328ab57ec5188e99284d7929b380030dc5cd
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-15 21:50:55 -0300

    month enum - keep legible code - js date object

---
+++ src/common/types/enums/month.enum.ts
@@ -0,0 +1,24 @@
+/**
+ * @enum Month
+ * Months enumeration - Used to keep a legible code, cause `JS Date` object use a different month convention.
+ * 
+ * Example:
+ * - Common convention: `january => 1 | december => 12`
+ * - `JS Date` convention: `january => 0 | december => 11`
+ */
+export enum Month {
+
+    January = 0,
+    February = 1,
+    March = 2,
+    April = 3,
+    May = 4,
+    June = 5,
+    July = 6,
+    August = 7,
+    September = 8,
+    October = 9,
+    November = 10,
+    December = 11
+
+}
