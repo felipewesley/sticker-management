99f94f4268f6778c062390c919e53a572a6d7195
Felipe Basso <fe.wesleybasso@gmail.com>
2022-10-16 23:27:55 -0300

    register cors handler - express app

--- index.ts
+++ index.ts
@@ -2,10 +2,14 @@ import { AppStartup } from './src/startup';
 import express, { Express } from 'express';
 import { errorHandler } from './src/core/handlers/error.handler';
 // import dotenv from 'dotenv';
+const cors = require('cors');
 
 const app: Express = express();
 
 // dotenv.config();
+
+// Register cors handler
+app.use(cors());
 
 const port = process.env.PORT ?? 3000;
 
